931627be24cff6792e7860da9d8ea2cede1bf536
ayubeay <ayubeay@yahoo.com>
2026-04-13 16:16:26 -0400

    feat: pass regime_board and governance_drift through API

--- verity_api_gated.py
+++ verity_api_gated.py
@@ -774,6 +774,8 @@ def helixcan_summary():
         "recent_governance": list(reversed(recent_gov)),
         "recent_challenges": recent_challenges,
         "cohorts": cohorts if cohorts else {},
+        "regime_board": snapshot.get("regime_board", {}),
+        "governance_drift": snapshot.get("governance_drift", {}),
         "updated_at": updated_at if updated_at else 0,
     }
 
